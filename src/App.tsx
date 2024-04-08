dec4e09f50d61303f7ccaedf3bd42ae2fdaaea9d
yamaryosan <canda3267@gmail.com>
2024-04-08 23:38:06 +0900

    [add] 文章表示コンポーネント

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useQuery } from 'react-query';
 import './App.css';
 
+// データ取得用関数
+const fetchSentence = async () => {
+  const res = await fetch('http://localhost/api/sentences');
+  if (!res.ok) {
+    throw new Error('サーバエラー');
+  }
+  return res.json() as Promise<Sentences>;
+};
+
+type Sentence = {
+  id: number;
+  sentence: string;
+  created_at: string;
+  updated_at: string;
+};
+
+type Sentences = Array<Sentence>;
+
 function App() {
+  // データを取得
+  const { data, isLoading, isError, error } = useQuery<Sentences, Error>('sentence', fetchSentence);
+  
+  if (isLoading) {
+    return (
+      <h1>読み込み中...</h1>
+    );
+  }
+
+  if (isError && error) {
+    return (
+      <h1>エラーが発生しました: {error.message}</h1>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        {data?.map(sentence => (
+          <li key={sentence.id}>
+            {sentence.sentence.split('\n').map((line, index) => (
+              <React.Fragment key={index}>
+                {line}
+                <br />
+              </React.Fragment>
+            ))}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
