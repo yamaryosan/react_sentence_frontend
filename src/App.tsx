302ae65ebb49cc090be4b01c2412900d8166b587
yamaryosan <canda3267@gmail.com>
2024-04-10 08:02:03 +0900

    [add] フッター追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './Footer';
 import Header from './Header';
 import { Outlet } from 'react-router-dom';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <Outlet />
+      <Footer />
     </div>
   )
 }
