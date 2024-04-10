eb4369cff77be80f2268a71f05a9e4e0edd69e33
yamaryosan <canda3267@gmail.com>
2024-04-10 21:54:14 +0900

    [create] メインコンテンツ部とサイドバーからなるエリアを作成

--- src/ContentsArea.tsx
+++ src/ContentsArea.tsx
@@ -1,12 +1,20 @@
+import MainContents from "./MainContents";
+import Sidebar from "./Sidebar";
+
+import Box from "@mui/material/Box";
+
 type ContentsAreaProps = {
     name: string;
 };
 
 function ContentsArea({name}: ContentsAreaProps) {
     return (
-        <div>
-        <p>ページ名は{name}です</p>
-        </div>
+        <>
+            <Box sx={{ display: 'flex' }}>
+                <MainContents name={name} />
+                <Sidebar />
+            </Box>
+        </>
     );
 }
 
