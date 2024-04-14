28413685678551933340a0ef012cf20fdd6631ca
yamaryosan <canda3267@gmail.com>
2024-04-14 22:23:56 +0900

    [add] 検索ワードをステートで管理

--- src/SearchWindow.tsx
+++ src/SearchWindow.tsx
@@ -1,6 +1,19 @@
+import { useState } from 'react';
 import { Box } from '@mui/material';
+import { ChangeEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function SearchWindow() {
+    const [keyword, setKeyword] = useState<string>('');
+    const navigate = useNavigate();
+
+    // 値を更新
+    const handleForm = (e: ChangeEvent<HTMLInputElement>) => {
+        setKeyword(e.target.value);
+    }
+
+    const handleSubmit = () => {navigate('/search/' + keyword)};
+
     return (
         <Box sx={{
             display: 'flex',
@@ -9,8 +22,8 @@ export default function SearchWindow() {
             backgroundColor: '#f0f0f0'
         }}>
             <form action="/search" method="get">
-                <input type="text" name="q" />
-                <input type="submit" value="検索" />
+                <input type="text" name="keyword" onChange={handleForm} />
+                <input type="button" value="検索" onClick={handleSubmit} />
             </form>
         </Box>
     )
