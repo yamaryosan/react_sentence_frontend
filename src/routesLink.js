06d5c398a6532ae2c7b3a7d30c30960d5ce94b08
yamaryosan <canda3267@gmail.com>
2024-04-22 22:34:02 +0900

    [add] 文章検索用パスを追加

--- src/routesLink.js
+++ src/routesLink.js
@@ -1,9 +1,10 @@
 import {Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import App from './App';
 import ContentsArea from './ContentsArea';
-import {New, Home, Recommendations, Contact} from './MainContents';
+import {New, Home, Contact} from './MainContents';
 import Article from './Article';
 import ResultArticles from './ResultArticles';
+import ResultSentences from './ResultSentences';
 
 const routes = createBrowserRouter(
     createRoutesFromElements(
@@ -13,6 +14,7 @@ const routes = createBrowserRouter(
             <Route path="/contact" element={<ContentsArea children={Contact} />}/>
             <Route path="/articles/:id" element={<ContentsArea children={<Article />} />}/>
             <Route path="/articles/search/:keyword" element={<ContentsArea children={<ResultArticles />} />}/>
+            <Route path="/sentences/search/:keyword" element={<ContentsArea children={<ResultSentences />} />}/>
         </Route>
     )
 )
