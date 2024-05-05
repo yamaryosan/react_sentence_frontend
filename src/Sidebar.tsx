fc5fd5e582314896bd07f50228ece62f1330d2c6
yamaryosan <canda3267@gmail.com>
2024-05-05 23:59:37 +0900

    [add] サイドバー新規記事を表示

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,9 +1,11 @@
 import AdminCheck from "./AdminCheck"
+import SidebarNewArticles from "./SidebarNewArticles"
 
 export default function Sidebar() {
     return (
         <div>
             <AdminCheck />
+            <SidebarNewArticles />
         </div>
     )
 };
