104648631a466c8061e2b1d9c0f8f382ee9a896a
yamaryosan <canda3267@gmail.com>
2024-05-06 19:11:53 +0900

    [add] ブレークポイントをカスタマイズ

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'xs': '0px',
+      'sm': '600px',
+      'md': '900px',
+      'lg': '1200px',
+      'xl': '1536px',
+    }
   },
   plugins: [],
 }
