b66e57b7954c9dc38b2b684e35cbb872b89c36ca
yamaryosan <canda3267@gmail.com>
2024-05-10 00:07:59 +0900

    [update] レイアウトを整理

--- src/ContentsArea.tsx
+++ src/ContentsArea.tsx
@@ -14,14 +14,17 @@ export default function ContentsArea({children}: ContentsAreaProps) {
                 flexDirection: {xs: 'column', md: 'row'},
                 px: 5,
                 pt: 5,
+                pb: 5,
                 fontSize: {xs: '1.2rem', md: '1rem'},
                 }}>
                 <Box sx={{ 
                     width: {xs: 1, md: 5/6},
                     textAlign: 'left',
                     pr: {xs: 0, md: 5},
+                    mr: {xs: 0, md: 5},
+                    backgroundColor: 'whitesmoke',
                     }}>
-                    {children}
+                        <Box sx={{p: 2}}>{children}</Box>
                 </Box>
                 <Box sx={{ 
                     width: {xs:1, md: 1/6} }}>
