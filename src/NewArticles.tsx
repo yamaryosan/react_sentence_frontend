3fc96548b94b1f1934cdd4c024734b7f5a054310
yamaryosan <canda3267@gmail.com>
2024-05-11 21:21:26 +0900

    [add] ページネーションを追加

--- src/NewArticles.tsx
+++ src/NewArticles.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {useQuery} from 'react-query';
 import Article from './Article';
 import Box from '@mui/material/Box';
+import MuiPagination from '@mui/material/Pagination';
 import {Link} from 'react-router-dom';
-import ThumbnailBox from './ThumbnailBox';
 
 type Article = {
     id: number;
@@ -28,6 +28,18 @@ export default function NewArticles() {
         () => fetchArticleItem()
     );
 
+    // ページネーション用の変数
+    const [page, setPage] = React.useState(1);
+    const pageSize = 10;
+
+    const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        setPage(value);
+    }
+
+    const indexOfLast = page * pageSize;
+    const indexOfFirst = indexOfLast - pageSize;
+    const currentArticles = articles?.slice(indexOfFirst, indexOfLast);
+
     if (isLoading) {
         return <div>読み込み中...</div>;
     }
@@ -37,7 +49,7 @@ export default function NewArticles() {
     }
     return (
         <Box className="flex-col">
-            {articles?.map((article) => (
+            {currentArticles?.map((article) => (
                 <Box key={article.id}
                 className=" bg-white pd-2 border border-transparent hover:border hover:border-blue-600 transition duration-2 mb-2">
                     <Link to={`/articles/${article.id}`}>
@@ -49,6 +61,13 @@ export default function NewArticles() {
                     </Link>
                 </Box>
             ))}
+                <MuiPagination
+                count={Math.ceil((articles?.length || 0) / pageSize)}
+                page={page}
+                onChange={handlePageChange}
+                size='large'
+                sx={{display: 'flex', justifyContent: 'center', pt: 2, pb: 2}}
+                />
         </Box>
     );
 }
