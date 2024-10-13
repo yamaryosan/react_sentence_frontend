f2442680d3b2e33a66f78bf847b5715dc65e1928
yamaryosan <canda3267@gmail.com>
2024-10-13 20:04:27 +0900

    [update] 検索窓の位置をヘッダーに変更

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,18 +1,8 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
 import LaptopIcon from '@mui/icons-material/Laptop';
-import SentenceSearchWindow from './sidebar/SentenceSearchWindow';
+import SentenceSearchWindow from './header/SentenceSearchWindow';
 
-import {Link} from 'react-router-dom';
 import { HomeOutlined, MailOutlined } from '@mui/icons-material';
 
 // ナビゲーションメニューの項目
