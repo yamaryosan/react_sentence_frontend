6c6fe62fc833a569795469b67c1ce0285a29a7fc
yamaryosan <canda3267@gmail.com>
2024-10-14 19:35:38 +0900

    [update] インポートを相対パスに変更

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,7 +1,6 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import LaptopIcon from '@mui/icons-material/Laptop';
-import VerificationProvider from '@/searchWindow/VerificationProvider';
 import { HomeOutlined, MailOutlined, ArticleOutlined } from '@mui/icons-material';
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -9,8 +8,8 @@ import { Link } from 'react-router-dom';
 // ナビゲーションメニューの項目
 const pages = [
     {name: 'Home', url: '/', icon: <HomeOutlined />},
-    {name: 'Contact', url: '/contact', icon: <MailOutlined />},
     {name: 'Articles', url: '/allArticles', icon: <ArticleOutlined />},
+    {name: 'Contact', url: '/contact', icon: <MailOutlined />},
 ];
 
 export default function Header() {
