e48288267021c338565ea1f4ab0a19977fe18338
yamaryosan <canda3267@gmail.com>
2024-10-14 19:07:29 +0900

    [add] コメントを追加

--- src/searchWindow/VerificationProvider.tsx
+++ src/searchWindow/VerificationProvider.tsx
@@ -4,6 +4,10 @@ import { fetchVerification } from "@/api/verify";
 import { useEffect, useState } from "react";
 import VerificationContext from "@/header/VerificationContext";
 
+/**
+ * 記事検索ウィンドウと文章検索ウィンドウを提供するコンポーネント
+ * 文章検索ウィンドウは認証済みの場合のみ表示される
+ */
 export default function VerificationProvider() {
     const [isVerified, setIsVerified] = useState(false);
 
