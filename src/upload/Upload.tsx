deb4b85b3046e2e31663e4e6b0b014dd1f18fa6b
yamaryosan <canda3267@gmail.com>
2024-10-14 14:04:21 +0900

    [update] インポートを相対パスに変更

--- src/upload/Upload.tsx
+++ src/upload/Upload.tsx
@@ -1,10 +1,10 @@
-import ArticleFilesUpload from "./ArticleFilesUpload";
-import ImagesUpload from "./ImagesUpload";
-import SentenceFileUpload from "./SentenceFileUpload";
-import ArticlesIndexModal from "./ArticlesIndexModal";
-import ArticleFilesDeleteModal from "./ArticleFilesDeleteModal";
-import ImagesDeleteModal from "./ImagesDeleteModal";
-import SentenceFileDeleteModal from "./SentenceFileDeleteModal";
+import ArticleFilesUpload from "@/upload/ArticleFilesUpload";
+import ImagesUpload from "@/upload/ImagesUpload";
+import SentenceFileUpload from "@/upload/SentenceFileUpload";
+import ArticlesIndexModal from "@/upload/ArticlesIndexModal";
+import ArticleFilesDeleteModal from "@/upload/ArticleFilesDeleteModal";
+import ImagesDeleteModal from "@/upload/ImagesDeleteModal";
+import SentenceFileDeleteModal from "@/upload/SentenceFileDeleteModal";
 
 export default function Upload() {
     return (
