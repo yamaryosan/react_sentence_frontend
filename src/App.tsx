1d43a552574c1b435ef447a62a8f62d5188e8235
yamaryosan <canda3267@gmail.com>
2024-10-14 16:16:01 +0900

    [add] MUIテーマを適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import './App.css';
-import Footer from './Footer';
-import Header from './Header';
+import '@/App.css';
+import Footer from '@/Footer';
+import Header from '@/header/Header';
 import { Outlet } from 'react-router-dom';
+import theme from '@/theme/index';
+
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Outlet />
-      <Footer />
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Header />
+            <Outlet />
+            <Footer />
+        </ThemeProvider>
     </div>
   )
 }
