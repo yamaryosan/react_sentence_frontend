5adb2c949bd93d4d0d9bee191c7857f301bc1121
yamaryosan <canda3267@gmail.com>
2024-10-14 19:40:57 +0900

    [update]　ディレクトリを変更

---
+++ src/component/ArticleCard.tsx
@@ -0,0 +1,31 @@
+import Box from "@mui/material/Box";
+import {Link} from 'react-router-dom';
+
+type ArticleCardProps = {
+    article: {
+        id: number;
+        title: string;
+        content: string;
+        imagePaths: string[];
+    }
+}
+
+/**
+ * 記事のカードを表示するコンポーネント
+ * @param article 記事
+ */
+export default function ArticleCard({article}: ArticleCardProps) {
+    return (
+        <Box key={article.id}
+        className=" bg-white pd-2 border border-transparent hover:border hover:border-blue-600 transition duration-2 mb-2">
+            <Link to={`/articles/${article.id}`}>
+                <div className="flex justify-center items-center h-[200px] overflow-hidden">
+                <img src={article.imagePaths[0]} alt="thumbnail" className="max-h-full max-w-full object-contain"/>
+                </div>
+                <h1 className="pt-2 px-4 text-center xs:text-xl md:text-2xl">{article.title}</h1>
+                <p className="px-4 text-lg xs:line-clamp-3 sm:line-clamp-5 md:line-clamp-6">{article.content}</p>
+            </Link>
+        </Box>
+    )
+
+}
