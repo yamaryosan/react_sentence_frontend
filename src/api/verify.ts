a4f2fd25c5b41c1b8f0ac17b13ecd4889225f8fd
yamaryosan <canda3267@gmail.com>
2024-10-17 21:45:32 +0900

    [fix] レスポンスの認証情報を正しく取得

--- src/api/verify.ts
+++ src/api/verify.ts
@@ -1,3 +1,7 @@
+type VerificationResponse = {
+    isVerified: string;
+}
+
 export async function fetchVerification(): Promise<boolean | undefined> {
     try {
         const apiUrl = process.env.REACT_APP_API_URL;
@@ -7,7 +11,9 @@ export async function fetchVerification(): Promise<boolean | undefined> {
         if (!response.ok) {
             throw new Error('認証情報の取得に失敗しました');
         }
-        return response.json() as Promise<boolean>;
+        const result = await response.json() as VerificationResponse;
+        const isVerified = result.isVerified === "true" ? true : false;
+        return isVerified;
     } catch (error) {
         if (error instanceof Error) {
             throw new Error(error.message);
