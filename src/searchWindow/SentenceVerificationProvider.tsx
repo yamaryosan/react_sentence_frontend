79e3d59ed248bdc50d9cd4fbcff61b485c4bf718
yamaryosan <canda3267@gmail.com>
2024-10-17 21:43:09 +0900

    [update] コンポーネント名を変更

--- src/searchWindow/SentenceVerificationProvider.tsx
+++ src/searchWindow/SentenceVerificationProvider.tsx
@@ -2,13 +2,13 @@ import ArticleSearchWindow from "@/searchWindow/ArticleSearchWindow";
 import SentenceSearchWindow from "@/searchWindow/SentenceSearchWindow";
 import { fetchVerification } from "@/api/verify";
 import { useEffect, useState } from "react";
-import VerificationContext from "@/header/VerificationContext";
+import SentenceVerificationContext from "@/searchWindow/SentenceVerificationContext";
 
 /**
  * 記事検索ウィンドウと文章検索ウィンドウを提供するコンポーネント
  * 文章検索ウィンドウは認証済みの場合のみ表示される
  */
-export default function VerificationProvider() {
+export default function SentenceVerificationProvider() {
     const [isVerified, setIsVerified] = useState(false);
 
     useEffect(() => {
@@ -17,12 +17,15 @@ export default function VerificationProvider() {
             setIsVerified(isVerified ?? false);
         }
         fetchData();
+
+        const intervalId = setInterval(fetchData, 5000);
+        return () => clearInterval(intervalId);
     }, []);
 
     return (
-        <VerificationContext.Provider value={isVerified}>
+        <SentenceVerificationContext.Provider value={isVerified}>
             <ArticleSearchWindow />
             {isVerified && <SentenceSearchWindow />}
-        </VerificationContext.Provider>
+        </SentenceVerificationContext.Provider>
     )
 }
