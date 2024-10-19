ea222eac67bfc4403399fb202145ce85eee063b8
yamaryosan <canda3267@gmail.com>
2024-10-19 14:44:02 +0900

    [update] コンポーネントのまとめ方を変更

--- src/upload/UploadForm.tsx
+++ src/upload/UploadForm.tsx
@@ -1,21 +1,17 @@
 import ArticleFilesUpload from "@/upload/ArticleFilesUpload";
 import ImagesUpload from "@/upload/ImagesUpload";
 import SentenceFileUpload from "@/upload/SentenceFileUpload";
-import ArticlesIndexModal from "@/upload/ArticlesIndexModal";
-import ArticleFilesDeleteModal from "@/upload/ArticleFilesDeleteModal";
-import ImagesDeleteModal from "@/upload/ImagesDeleteModal";
-import SentenceFileDeleteModal from "@/upload/SentenceFileDeleteModal";
+import DeleteButtons from "@/upload/DeleteButtons";
+import IndexButton from "@/upload/IndexButton";
 
 export default function UploadForm() {
     return (
-        <div>
+        <>
             <ArticleFilesUpload />
             <ImagesUpload />
             <SentenceFileUpload />
-            <ArticlesIndexModal />
-            <ArticleFilesDeleteModal />
-            <ImagesDeleteModal />
-            <SentenceFileDeleteModal />
-        </div>
+            <DeleteButtons />
+            <IndexButton />
+        </>
     )
 };
