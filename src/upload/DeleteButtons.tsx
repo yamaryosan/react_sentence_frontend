b5a04e44bef6d97323cab41b3d34853b9be3d823
yamaryosan <canda3267@gmail.com>
2024-10-19 17:41:49 +0900

    [update] 使用するコンポーネントを変更

--- src/upload/DeleteButtons.tsx
+++ src/upload/DeleteButtons.tsx
@@ -1,16 +1,19 @@
 import { Box } from "@mui/material";
-import ArticleFilesDeleteModal from "@/upload/ArticleFilesDeleteModal";
-import ImagesDeleteModal from "@/upload/ImagesDeleteModal";
-import SentenceFileDeleteModal from "@/upload/SentenceFileDeleteModal";
+import ArticleFilesDeleteButton from "@/upload/ArticleFilesDeleteButton";
+import ImagesDeleteButton from "@/upload/ImagesDeleteButton";
+import SentenceFileDeleteButton from "@/upload/SentenceFileDeleteButton";
 
+/**
+ * 削除ボタンをまとめたコンポーネント
+ */
 export default function DeleteButtons() {
     return (
         <>
             <h3>削除</h3>
             <Box sx={{ display: 'flex', flexDirection: 'column', border: '1px solid #e0e0e0', padding: '1rem', borderRadius: '0.5rem' }}>
-                <ArticleFilesDeleteModal />
-                <ImagesDeleteModal />
-                <SentenceFileDeleteModal />
+                <ArticleFilesDeleteButton />
+                <ImagesDeleteButton />
+                <SentenceFileDeleteButton />
             </Box>
         </>
     )
