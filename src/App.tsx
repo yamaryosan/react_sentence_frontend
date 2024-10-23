dc59570c492bc9394dbd8c26af1a335281b8021a
yamaryosan <canda3267@gmail.com>
2024-10-24 07:47:50 +0900

    [add] デバイス種類判別のプロバイダで全体をくくった

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import Header from '@/header/Header';
 import { Outlet } from 'react-router-dom';
 import theme from '@/theme/index';
 import ArticleSearchWindow from '@/searchWindow/ArticleSearchWindow';
+import DeviceTypeProvider from '@/hooks/DeviceTypeProvider';
 
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Header />
-            <ArticleSearchWindow />
-            <Outlet />
-            <Footer />
-        </ThemeProvider>
+        <DeviceTypeProvider>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Header />
+                <ArticleSearchWindow />
+                <Outlet />
+                <Footer />
+            </ThemeProvider>
+        </DeviceTypeProvider>
     </div>
     );
 }
