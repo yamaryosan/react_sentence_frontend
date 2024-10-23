40ee882aee577c5c8d7702609298a5d889166f58
yamaryosan <canda3267@gmail.com>
2024-10-24 07:43:13 +0900

    [create] デバイスの種類を判別するプロバイダを作成

---
+++ src/hooks/DeviceTypeContext.tsx
@@ -0,0 +1,11 @@
+import { createContext } from 'react';
+
+type DeviceType = 'desktop' | 'mobile';
+
+/**
+ * デバイスタイプのコンテキスト
+ * 画面幅によってデバイスの種類を判定する
+ */
+const DeviceTypeContext = createContext<DeviceType>('desktop');
+
+export default DeviceTypeContext;
