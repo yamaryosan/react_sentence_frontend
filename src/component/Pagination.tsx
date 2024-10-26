06c1fcc704266b2dfac535b3b4224563bea34cd2
yamaryosan <canda3267@gmail.com>
2024-10-27 00:00:45 +0900

    [fix] 引数を変更

--- src/component/Pagination.tsx
+++ src/component/Pagination.tsx
@@ -2,18 +2,18 @@ import MuiPagination from '@mui/material/Pagination';
 import { useContext } from 'react';
 import DeviceTypeContext from '@/hooks/DeviceTypeContext';
 
-type PaginationProps<T> = {
-    items: T[];
+type PaginationProps = {
+    total: number;
     pageSize: number;
     page: number;
     setPage: (page: number) => void;
 }
 
-export default function Pagination<T>({items, pageSize, page, setPage}: PaginationProps<T>) {
+export default function Pagination({total, pageSize, page, setPage}: PaginationProps) {
     const deviceType = useContext(DeviceTypeContext);
     return (
         <MuiPagination
-            count={Math.ceil((items?.length || 0) / pageSize)}
+            count={Math.ceil(total / pageSize)}
             page={page}
             onChange={(e, value) => setPage(value)}
             size={deviceType === 'desktop' ? 'large' : 'medium'}
