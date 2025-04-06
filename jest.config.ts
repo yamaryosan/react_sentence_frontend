5da4c201bc538a1c05ca2c4b06f7671fc6760d26
yamaryosan <canda3267@gmail.com>
2025-04-06 20:56:53 +0900

    add: Jest用configを作成

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import { Config } from "jest";
+
+const config: Config = {
+    preset: "ts-jest",
+    testEnvironment: "jsdom",
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1",
+        "^@/component/(.*)$": "<rootDir>/src/component/$1",
+        "^@/api/(.*)$": "<rootDir>/src/api/$1",
+        "^@/hooks/(.*)$": "<rootDir>/src/hooks/$1",
+        "^@/tests/(.*)$": "<rootDir>/src/tests/$1",
+    },
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest",
+    },
+};
+
+export default config;
